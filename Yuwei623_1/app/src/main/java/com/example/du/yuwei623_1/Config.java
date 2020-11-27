1a23c7730b5102bec8db55f3f56f7262ec2a0aeb
xsh8757398 <40623980+xsh8757398@users.noreply.github.com>
2020-11-27 22:35:49 +0800

    Update Config.java

--- Yuwei623_1/app/src/main/java/com/example/du/yuwei623_1/Config.java
+++ Yuwei623_1/app/src/main/java/com/example/du/yuwei623_1/Config.java
@@ -8,17 +8,17 @@ public class Config {
     /**
      * url前半部分
      */
-    public static final String BASE_URL = "https://api.miaodiyun.com/20150822";
+    public static final String BASE_URL = "https://api.miaodiyun.com/";
 
     /**
      * 开发者注册后系统自动生成的账号，可在官网登录后查看
      */
-    public static final String ACCOUNT_SID = "f988ae92e1f44118aa51717d54ab05b0";
+    public static final String ACCOUNT_SID = "";
 
     /**
      * 开发者注册后系统自动生成的TOKEN，可在官网登录后查看
      */
-    public static final String AUTH_TOKEN = "e723a5a752354ed8a837288a7947dfdb";
+    public static final String AUTH_TOKEN = "";
 
     /**
      * 响应数据类型, JSON或XML
